43ec02e5c9962a9c6e53ad559d552bdfca03dfde
Gustavo Heimburg Pereira <gutogustavogu@gmail.com>
2023-10-02 20:31:55 -0300

    Initial commit

---
+++ src/main/java/com/senac/SenacTeste/Pessoa.java
@@ -0,0 +1,11 @@
+package com.senac.SenacTeste;
+
+public class Pessoa {
+    private String nome;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/main/java/com/senac/SenacTeste/PessoaController.java
@@ -0,0 +1,27 @@
+package com.senac.SenacTeste;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class PessoaController{
+    private List<Pessoa> pessoaList = new ArrayList<>();
+
+    @GetMapping("/")
+    public String inicial(Model model){
+        model.addAttribute("pessoas", model);
+        return "index";
+    }
+
+public String adicionaPessoas(String nome){
+
+        
+
+        return "";
+}
+
+}
